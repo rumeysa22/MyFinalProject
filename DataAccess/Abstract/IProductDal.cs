e388c8d77cb14d555e43cd3056116fcc3e91c865
rumeysa <rumeysaakkaya22@gmail.com>
2023-11-22 02:20:29 +0300

    concrete

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -10,5 +10,9 @@ namespace DataAccess.Abstract
     public interface IProductDal
     {
         List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        List<Product> GetAllByCategory(int  categoryId);
     }
 }
