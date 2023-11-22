ec5c5f09fbf0892a90d2929c0f004f041dc87c62
rumeysa <rumeysaakkaya22@gmail.com>
2023-11-22 08:29:26 +0300

    DataAccess

--- DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -15,11 +15,11 @@ namespace DataAccess.Concrete.InMemory
         {
             _products = new List<Product>
             {
-                new Product{ProductId =1, CategoryId = 1, ProductName = "Bardak", UnitPrice = 15, UnıtInStock = 15},
-                new Product{ProductId =2, CategoryId = 1, ProductName = "Kamere", UnitPrice = 500, UnıtInStock = 3},
-                new Product{ProductId =3, CategoryId = 2, ProductName = "Telefon", UnitPrice = 1500, UnıtInStock = 2},
-                new Product{ProductId =4, CategoryId = 2, ProductName = "Klavye", UnitPrice = 150, UnıtInStock = 65},
-                new Product{ProductId =5, CategoryId = 2, ProductName = "Fare", UnitPrice = 85, UnıtInStock = 1}
+                new Product{ProductId =1, CategoryId = 1, ProductName = "Bardak", UnitPrice = 15, UnitInStock = 15},
+                new Product{ProductId =2, CategoryId = 1, ProductName = "Kamera", UnitPrice = 500, UnitInStock = 3},
+                new Product{ProductId =3, CategoryId = 2, ProductName = "Telefon", UnitPrice = 1500, UnitInStock = 2},
+                new Product{ProductId =4, CategoryId = 2, ProductName = "Klavye", UnitPrice = 150, UnitInStock = 65},
+                new Product{ProductId =5, CategoryId = 2, ProductName = "Fare", UnitPrice = 85, UnitInStock = 1}
 
             };
         }
