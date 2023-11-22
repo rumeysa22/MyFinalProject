b2e3fd838b8807696bdd4c58e6f3c4cca56c504a
rumeysa <rumeysaakkaya22@gmail.com>
2023-11-22 03:34:24 +0300

    consoleUI

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProductManager productManager = new ProductManager(new InMemoryProductDal());
+			foreach (var product in productManager.GetAll())
+			{
+                Console.WriteLine(product.ProductName);
+            }
+		}
+	}
+}
